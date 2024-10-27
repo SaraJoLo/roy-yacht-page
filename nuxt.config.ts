5c0dbf88f3dd400f23e4644674a5aecaa3ca2001
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-10-27 12:31:15 +0100

    added dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   srcDir: 'src',
-  modules: ['nuxt-icon'],
-  css: ['~/assets/main.scss'],
-  plugins: ['~/plugins/fontawesome.js'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
