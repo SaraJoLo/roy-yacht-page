1a758f49a66354c32ad0f8a88ca895c137d0b304
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-10-27 14:02:23 +0100

    feature nav bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <header class="navbar">
+    <div class="royBox">
+      <img src="public/Roy.png" alt="Logo" />
+    </div>
+    <div class="titleBox">YACHTS FOR SALE · 340</div>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: #fff;
+  padding: 10px 20px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.royBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 70px;
+  }
+}
+
+.titleBox {
+  font-size: 1.2rem;
+}
+</style>
